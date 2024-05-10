51b4f4a23f8e7684c7763d093696a9f68a705a77
Dilbar Akkaya <dilbar_akkaya@epam.com>
2024-05-10 03:11:29 +0300

    feat: integrate with s3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://ytru5xy4cd.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://cruvka2bu8.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://ytru5xy4cd.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
